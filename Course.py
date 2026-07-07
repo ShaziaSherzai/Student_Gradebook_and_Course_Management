996234db55cc8a0d99b46dd16502b805e2b7c58a
ShaziaSherzai <shazia.sherzai2020@gmail.com>
2026-07-08 03:20:31 +0430

    Course class created

---
+++ Course.py
@@ -0,0 +1,6 @@
+class Course:
+    def __init__(self, course_code, course_name, students, assignments):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.students = students
+        self.assignments = assignments
