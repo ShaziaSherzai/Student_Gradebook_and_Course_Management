3c4cb96814e78b6ba7bd5c7649746347390b4652
ShaziaSherzai <shazia.sherzai2020@gmail.com>
2026-07-09 00:01:22 +0430

    Methods added to the course class

--- Course.py
+++ Course.py
@@ -4,3 +4,21 @@ class Course:
         self.course_name = course_name
         self.students = students
         self.assignments = assignments
+
+    def add_student(self, student_id):
+        self.students.append(student_id)
+
+    def add_assessment(self, assessment):
+        self.assignments.append(assessment)
+
+    def find_assignments(self, title):
+        for assessment in self.assessments:
+            if assessment.title == title:
+                return assessment
+        return None
+
+    def display_info(self):
+        print(f"course code: {self.course_code}")
+        print(f"course name: {self.course_name}")
+        print(f"students: {len(self.students)}")
+        print(f"assignments: {len(self.assignments)}")
