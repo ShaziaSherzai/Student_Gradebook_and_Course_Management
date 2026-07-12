a3e5f0c253933a6952d5c00bf560a10f140d8056
ShaziaSherzai <shazia.sherzai2020@gmail.com>
2026-07-12 21:41:03 +0430

    Exam class created

---
+++ Exam.py
@@ -0,0 +1,16 @@
+import Assessment as Assessment
+
+class Exam(Assessment):
+    def __init__(self, title, score):
+        super().__init__(title, score)
+
+    def display_info(self):
+        return f"Exam: {self.title} - Max_score: {self.max_score}"
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 55:
+            return "Passed exam!"
+        else:
+            return "Failed exam!"
+        
