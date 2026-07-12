c527a4fd394b6332dfcbc6461c4f60eb51b35eb4
ShaziaSherzai <shazia.sherzai2020@gmail.com>
2026-07-12 20:53:28 +0430

    Quiz class created

---
+++ Quiz.py
@@ -0,0 +1,21 @@
+import Assessment as Assessment
+
+class Quiz(Assessment):
+    def __init__(self, title, max_score):
+        super().__init__(title, max_score)
+
+    def display_info(self):
+        return f"Quiz: {self.title} - max_score: {self.max_score}"
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 90:
+            return "Excellent quiz result!"
+        elif percentage >= 80:
+            return "Great quiz result!"
+        elif percentage >= 70:
+            return "Good quiz result!"
+        elif percentage >= 40:
+            return "Keep practicing!"
+        else:
+            return "You need more practicing!"
